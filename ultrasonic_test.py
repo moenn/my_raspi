98f63f391bda8459ad718f9b0b1180a54e0d6a4b
moenn <dance.jms@gmail.com>
2018-12-03 23:30:12 +0800

    Update ultrasonic_test.py

--- ultrasonic_test.py
+++ ultrasonic_test.py
@@ -1,34 +1,53 @@
+#导入 GPIO库
 import RPi.GPIO as GPIO
 import time
-
-print('Distance Measurement In Progress')
-
+ 
+#设置 GPIO 模式为 BCM
 GPIO.setmode(GPIO.BCM)
-TRIG = 2
-ECHO = 3
-GPIO.setup(TRIG,GPIO.OUT)
-GPIO.setup(ECHO,GPIO.IN)
-
-# 发送 trig 信号  持续 10us 的方波脉冲
-GPIO.output(TRIG,True)
-time.sleep(0.00001)
-GPIO.output(TRIG,False)
-
-
-# 等待低电平结束，然后记录时间。
-while GPIO.input(ECHO) == 0：
-	pass
-pulse_start = time.time()
-
-# 等待高电平结束，然后记录时间。
-while GPIO.input(ECHO) == 1:
-	pass
-pulse_end = time.time()
-
-
-pulse_duration = pulse_end - pulse_start
-distance = pulse_duration * 17150
-distance = round(distance,2)
-print("Distance: {}cm".format(distance))
-GPIO.cleanup()
+ 
+#定义 GPIO 引脚
+GPIO_TRIGGER = 23
+GPIO_ECHO = 24
+ 
+#设置 GPIO 的工作方式 (IN / OUT)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+ 
+def distance():
+    # 发送高电平信号到 Trig 引脚
+    GPIO.output(GPIO_TRIGGER, True)
+ 
+    # 持续 10 us 
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+ 
+    start_time = time.time()
+    stop_time = time.time()
+ 
+    # 记录发送超声波的时刻1
+    while GPIO.input(GPIO_ECHO) == 0:
+        start_time = time.time()
+ 
+    # 记录接收到返回超声波的时刻2
+    while GPIO.input(GPIO_ECHO) == 1:
+        stop_time = time.time()
+ 
+    # 计算超声波的往返时间 = 时刻2 - 时刻1
+    time_elapsed = stop_time - start_time
+    # 声波的速度为 343m/s， 转化为cm。得到距离
+    distance = (time_elapsed * 34300) / 2
+ 
+    return distance
+ 
+if __name__ == '__main__':
+    try:
+        while True:
+            dist = distance()
+            print("Measured Distance = {} cm".format(dist))
+            time.sleep(1)
+ 
+        # Reset by pressing CTRL + C
+    except KeyboardInterrupt:
+        print("Measurement stopped by User")
+        GPIO.cleanup()
 
